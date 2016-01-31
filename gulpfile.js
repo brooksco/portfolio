7640ae152ba2489ab334a49c784fd6647c53756e
Colin Brooks <colinbooks@gmail.com>
2016-01-30 22:39:52 -0500

    Redid images, resized, and all kinds of other tweaks.

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,7 @@ var sassPaths = [
 var merge = require('merge-stream');
 var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
-
-gulp.task('css', function(){
-  return gulp.src('css/*.css')
-    .pipe(concat('app.min.css'))
-    .pipe(gulp.dest('css'));
-});
+var uglify = require('gulp-uglify');
 
 
 gulp.task('sass', function() {
@@ -30,7 +25,7 @@ gulp.task('sass', function() {
       browsers: ['last 2 versions', 'ie >= 9']
     }));
 
-    cssStream = gulp.src('css/*.css')
+    cssStream = gulp.src(['css/app.css', 'css/lightbox.css'])
       .pipe(cssnano());
 
     return merge(sassStream, cssStream)
@@ -39,7 +34,15 @@ gulp.task('sass', function() {
 
 });
 
+gulp.task('js', function() {
+  return gulp.src(['js/masonary.min.js', 'js/enquire.min.js', 'js/jquery.waypoints.min.js', 'js/lightbox.min.js', 'js/app.js'])
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'))
+});
+
 
 gulp.task('default', ['sass'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['js/app.js'], ['js']);
 });
--- js/app.js
+++ js/app.js
@@ -6,56 +6,98 @@ $(document).ready(function() {
 	// So replace the videos with images
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		$("#main-left-video, #main-right-video").remove();
- 		$("#main-left").css({ "background" : "url(img/code.jpg) no-repeat bottom center", "background-size" : "cover" });
- 		$("#main-right").css({ "background" : "url(img/rainierunderthemoon.jpg) no-repeat bottom center", "background-size" : "cover" });
- 		$("#artdev-video").replaceWith("<img src='img/artdev_loop.gif' alt='' />");
+		$("#main-left").css({ "background" : "url(img/code.jpg) no-repeat bottom center", "background-size" : "cover" });
+		$("#main-right").css({ "background" : "url(img/rainierunderthemoon_1920.jpg) no-repeat bottom center", "background-size" : "cover" });
+		$("#artdev-video").replaceWith("<img src='img/artdev_loop.gif' alt='' />");
+
+	} else {
+		// Otherwise fade in the videos
+		$("#main-left-video, #main-right-video").css('opacity', 0).animate( { opacity: 1 }, 400);
 	}
+
+	
 
 	// Index page
 	// Handlers for changing brightness on hover (mouse enter and mouse leave)
 	$( "#main-left" ).mouseenter(function() {
-  		$("#main-right").css({"filter" : "brightness(50%)", "-webkit-filter" : "brightness(50%)" });
-  		$("#main-right-video")[0].pause();
-  		$("#main-left-video")[0].play();
+		$("#main-right").css({"filter" : "brightness(50%)", "-webkit-filter" : "brightness(50%)" });
+		$("#main-right-video")[0].pause();
+		$("#main-left-video")[0].play();
 	});
 
 	$( "#main-left" ).mouseleave(function() {
-  		$("#main-right").css({"filter" : "brightness(100%)", "-webkit-filter" : "brightness(100%)" });
-  		$("#main-right-video")[0].pause();
-  		$("#main-left-video")[0].pause();
+		$("#main-right").css({"filter" : "brightness(100%)", "-webkit-filter" : "brightness(100%)" });
+		$("#main-right-video")[0].pause();
+		$("#main-left-video")[0].pause();
 	});
 
 	$( "#main-right" ).mouseenter(function() {
-  		$("#main-left").css({"filter" : "brightness(50%)", "-webkit-filter" : "brightness(50%)" });
-  		$("#main-left-video")[0].pause();
-  		$("#main-right-video")[0].play();
+		$("#main-left").css({"filter" : "brightness(50%)", "-webkit-filter" : "brightness(50%)" });
+		$("#main-left-video")[0].pause();
+		$("#main-right-video")[0].play();
 	});
 
 	$( "#main-right" ).mouseleave(function() {
-  		$("#main-left").css({"filter" : "brightness(100%)", "-webkit-filter" : "brightness(100%)" });
-  		$("#main-right-video")[0].pause();
-  		$("#main-left-video")[0].pause();
-	});
-
-	// Development page
-
-	$( ".dev-video" ).mouseenter(function() {
-  		$(this)[0].play();
-	});
-
-	$( ".dev-video" ).mouseleave(function() {
-  		$(this)[0].pause();
+		$("#main-left").css({"filter" : "brightness(100%)", "-webkit-filter" : "brightness(100%)" });
+		$("#main-right-video")[0].pause();
+		$("#main-left-video")[0].pause();
 	});
 
 	// Photography page
 
 	$(".item").each(function(i){
-		// $(this).css("opacity", "1");
-		// alert(i);
 		var current = $(this);
-    	setTimeout(function() { 
-    		current.css("opacity", "1");
-    	}, (i + 1) * 80);
+		setTimeout(function() { 
+			current.css("opacity", "1");
+		}, (i + 1) * 80);
 	});
+
+	// Development page
+
+    // Scrolling navbar for non-mobile sizes
+    enquire.register("screen and (min-width: 640px)", {
+    	match : function() {
+			if ($("#waypoint").length != 0) {
+
+				var waypoint = new Waypoint({
+					element: $('#waypoint'),
+					handler: function(direction) {
+						if (direction == 'down') {
+							$("#full-nav").fadeIn();
+						} else {
+							$("#full-nav").fadeOut();
+						}
+
+					},
+					offset: 120 
+				});
+			}
+		},  
+		unmatch : function() {
+			// $("#full-nav").hide();
+		}
+
+	});
+
+	// Save this so we don't havae to keep getting it
+	var $root = $('html, body');
+
+	// For the active link and my name, prevent it from reloading the page and instead scroll to the top
+	$("#full-nav .active, #full-nav-name").on('click', function(event){     
+		event.preventDefault();
+
+		$root.animate({
+			scrollTop: $("body").offset().top
+		}, 1000);
+	});
+
+	$( ".dev-video" ).mouseenter(function() {
+		$(this)[0].play();
+	});
+
+	$( ".dev-video" ).mouseleave(function() {
+		$(this)[0].pause();
+	});
+
 
 });
