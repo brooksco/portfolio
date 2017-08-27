72610060f1435d793986635145bf5f786aba1360
Colin Brooks <colinbooks@gmail.com>
2017-08-27 00:51:28 -0700

    updated to use nunjucks templating

--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,60 @@ var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+'node_modules/foundation-sites/scss',
+'bower_components/motion-ui/src'
 ];
 
 var merge = require('merge-stream');
 var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 var uglify = require('gulp-uglify');
+var nunjucksRender = require('gulp-nunjucks-render');
 
-
+// Stylesheets
 gulp.task('sass', function() {
   var sassStream, cssStream;
 
-  sassStream = gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed'
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }));
+  sassStream = gulp.src('scss/app.scss').pipe($.sass({
+    includePaths: sassPaths,
+    outputStyle: 'compressed'
+  })
+  .on('error', $.sass.logError))
+  .pipe($.autoprefixer({
+    browsers: ['last 2 versions', 'ie >= 9']
+  }));
 
-    cssStream = gulp.src(['css/app.css', 'css/lightbox.css'])
-      .pipe(cssnano());
+  cssStream = gulp.src(['css/app.css', 'css/lightbox.css'])
+  .pipe(cssnano());
 
-    return merge(sassStream, cssStream)
-      .pipe(concat('app.min.css'))
-      .pipe(gulp.dest('css'));
+  return merge(sassStream, cssStream)
+  .pipe(concat('app.min.css'))
+  .pipe(gulp.dest('css'));
 
 });
 
+// Javascript
 gulp.task('js', function() {
-  return gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/what-input/what-input.js', 'bower_components/foundation-sites/dist/foundation.js', 'js/masonary.min.js', 'js/enquire.min.js', 'js/jquery.waypoints.min.js', 'js/lightbox.min.js', 'js/app.js'])
-    .pipe(concat('app.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('js'))
+  return gulp.src(['bower_components/jquery/dist/jquery.js', 'node_modules/what-input/what-input.js', 'node_modules/foundation-sites/dist/foundation.js', 'js/masonary.min.js', 'js/enquire.min.js', 'js/jquery.waypoints.min.js', 'js/lightbox.min.js', 'js/app.js'])
+  .pipe(concat('app.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('js'))
 });
 
+// Partials
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('html/**')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['html/']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('./'))
+});
 
 gulp.task('default', ['sass'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
   gulp.watch(['js/app.js'], ['js']);
+  gulp.watch(['html/**'], ['nunjucks']);
 });
